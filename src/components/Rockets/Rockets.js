b70e3479a02676399fa7b4961a50963bd40bc928
abenaTenkorang <abenagy16@gmail.com>
2023-04-10 22:40:59 +0000

    Add component that renders list in UI

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,11 +1,29 @@
-import './Rockets.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import Rocket from './RocketItem';
+import { getRocketsAsync } from '../../store/rocket/rocketSlice';
+import './Rockets.css';
 
-function Rockets() {
+export default function Rockets() {
+  const { rockets, isLoading, error } = useSelector((state) => state.rockets);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRocketsAsync());
+  }, [dispatch]);
+
+  const listRockets = rockets.map((rocket) => (
+    <li key={rocket.id}>
+      <Rocket rocket={rocket} />
+    </li>
+  ));
+
   return (
     <div>
-      <h1>Rockets</h1>
+      {isLoading && <h2 className="loading">Loading...</h2>}
+      {error && <p className="error">{error}</p>}
+      <ul className="list">{listRockets}</ul>
     </div>
   );
 }
-
-export default Rockets;
