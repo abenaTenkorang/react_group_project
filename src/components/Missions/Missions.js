9401b8f9f8f818d2ab6b2c7f22e0a86bdab06d55
Abigail Boateng <109290913+abenaTenkorang@users.noreply.github.com>
2023-04-11 08:14:16 +0000

    Revert "Fetch missions data from SpaceX API"

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,66 +1,11 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  fetchMissions,
-  joinMission,
-  leaveMission,
-} from '../../redux/missions/missions';
-import './Missions.css';
+import './Missions.module.css';
 
-export default function Missions() {
-  const missions = useSelector((state) => state.missions);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (!missions.length) dispatch(fetchMissions());
-  }, [dispatch, missions.length]);
-
+function Missions() {
   return (
-    <section className="container">
-      <h2>Missions</h2>
-      <table className="missionTable">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {missions.map((mission) => (
-            <tr key={mission.id}>
-              <td>{mission.name}</td>
-              <td className="font-sm">{mission.description}</td>
-              <td className="mission-status-cell">
-                {!mission.joined && (
-                  <>
-                    <span className="status-badge">Not a member</span>
-                    <button
-                      type="button"
-                      className="action-button join-button"
-                      onClick={() => dispatch(joinMission(mission.id))}
-                    >
-                      Join Mission
-                    </button>
-                  </>
-                )}
-                {mission.joined && (
-                  <>
-                    <span className="status-badge joined">Active Member</span>
-                    <button
-                      type="button"
-                      className="action-button leave-button"
-                      onClick={() => dispatch(leaveMission(mission.id))}
-                    >
-                      Leave Mission
-                    </button>
-                  </>
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </section>
+    <div>
+      <h1>Missions</h1>
+    </div>
   );
 }
+
+export default Missions;
